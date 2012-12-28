8e5d970ce26eebbbab8256c06e921c3421ee3546
Victor Efimov <victor@vsespb.ru>
2012-12-28 21:49:19 +0400

    added checking for nil?

--- jekyll-cut.rb
+++ jekyll-cut.rb
@@ -1,6 +1,7 @@
 module Jekyll
     module CutFilter
         def cut(input)
+            return nil if input.nil?
             text_above_cut, text_below_cut = input.split '<cut />'
             return text_above_cut
         end
